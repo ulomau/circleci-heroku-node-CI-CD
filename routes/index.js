303557cba1b32d6d1119d4304d2d537d2f6b7bc2
Uloma <66249264+ulomau@users.noreply.github.com>
2021-03-31 08:23:07 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Hey! this is Uloma, trying out CI/CD on my deployed Node app on Heroku' });
 });
 
 module.exports = router;
